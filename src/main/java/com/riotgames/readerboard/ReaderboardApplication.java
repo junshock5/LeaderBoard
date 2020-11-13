66ee839c96a7cd95cd099508b195d648edad236a
junshock5 <61732452+junshock5@users.noreply.github.com>
2020-11-13 18:08:35 +0900

    create maven project with java8

---
+++ src/main/java/com/riotgames/readerboard/ReaderboardApplication.java
@@ -0,0 +1,13 @@
+package com.riotgames.readerboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReaderboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReaderboardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/riotgames/readerboard/ReaderboardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.riotgames.readerboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReaderboardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
