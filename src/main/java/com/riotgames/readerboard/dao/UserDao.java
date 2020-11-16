70d75155433faf8524c55c99fa5a7ea3956a5ea4
junshock5 <61732452+junshock5@users.noreply.github.com>
2020-11-16 16:01:48 +0900

    #4 계층 모델 추가 및 관련 API 추가 - HashMap과 ArrayList를 이용해서 Collections.sort하여 내림차순 정렬 후 티어와 순위 산정하는 로직 추가

--- src/main/java/com/riotgames/readerboard/dao/UserDao.java
+++ src/main/java/com/riotgames/readerboard/dao/UserDao.java
@@ -5,9 +5,11 @@ import com.riotgames.readerboard.mapper.UserMapper;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
+
 import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
 import java.io.*;
+import java.util.*;
 
 @Repository
 @Log4j2
@@ -22,7 +24,6 @@ public class UserDao {
         this.userMapper = userProfileMapper;
     }
 
-    // 상위 25000개 User 정보 mysql에 insert 만약 id값이 있다면 수정
     @PostConstruct
     public void init() throws IOException {
         loadInitialData();
@@ -34,21 +35,47 @@ public class UserDao {
 
     public void loadInitialData() throws IOException {
         try (BufferedReader in = new BufferedReader(new FileReader(FILE_DIRECTORY_NAME + "InitialData.txt"))) {
+            Map<Long, Integer> userMap = new HashMap<Long, Integer>();
             String line;
             while ((line = in.readLine()) != null) {
                 String[] pair = line.split(",");
                 long id = Long.parseLong(pair[0]);
                 int mmr = Integer.parseInt(pair[1]);
+                userMap.put(id, mmr);
+            }
+            List<Long> keySetList = new ArrayList<>(userMap.keySet());
+            // 내림 차순 정렬
+            Collections.sort(keySetList, (o1, o2) -> (userMap.get(o2).compareTo(userMap.get(o1))));
 
+            int i = 1;
+            for (Long key : keySetList) {
+                UserDTO.Tier tier;
+                if (i <= 100) {
+                    tier = UserDTO.Tier.CHALLENGER;
+                } else if ((double)i / (double)userMap.size() * 100 <= 1) {
+                    tier = UserDTO.Tier.MASTER;
+                } else if ((double)i / (double)userMap.size() * 100 <= 5 && (double)i / (double)userMap.size() * 100 > 1) {
+                    tier = UserDTO.Tier.DIAMOND;
+                } else if ((double)i / (double)userMap.size() * 100 <= 10 && (double)i / (double)userMap.size() * 100 > 5) {
+                    tier = UserDTO.Tier.PLATINUM;
+                } else if ((double)i / (double)userMap.size() * 100 <= 25 && (double)i / (double)userMap.size() * 100 > 10) {
+                    tier = UserDTO.Tier.GOLD;
+                } else if ((double)i / (double)userMap.size() * 100 <= 65 && (double)i / (double)userMap.size() * 100 > 25) {
+                    tier = UserDTO.Tier.SILVER;
+                } else {
+                    tier = UserDTO.Tier.BROZNE;
+                }
 
                 UserDTO userDTO = UserDTO.builder()
-                        .id(id)
-                        .matchMakerRank(mmr)
+                        .id(key)
+                        .matchMakerRank(userMap.get(key))
+                        .rank(i++)
+                        .tier(tier)
                         .build();
 
-                if (userMapper.idCheck(id) == 1) { // 이미 있는 id이기 떄문에 mmr값이 같은지 확인하고 없다면 update
+                if (userMapper.idCheck(key) == 1) { // 이미 있는 id이기 때때문에 mmr값이 같은지 확인고 없다면 update
                     userMapper.updateUser(userDTO);
-                }else{ // id값이 없기에 그냥 insert
+                } else { // id값이 없기에 그냥 insert
                     int insertCount = userMapper.register(userDTO);
                     if (insertCount != 1) {
                         log.error("insertMember ERROR! {}", userDTO);
@@ -56,8 +83,8 @@ public class UserDao {
                                 "insertUser ERROR! 유저 추가 메서드를 확인해주세요\n" + "Params : " + userDTO);
                     }
                 }
-
             }
+
         }
     }
 
